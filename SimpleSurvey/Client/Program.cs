990595d00e8e7a3d8c3a843b6a442f709b189543
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-09 14:58:13 +0300

    Client side, signalR connection, surveyHttpClient

--- SimpleSurvey/Client/Program.cs
+++ SimpleSurvey/Client/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SimpleSurvey.Shared;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -17,7 +20,18 @@ namespace SimpleSurvey.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            var baseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = baseAddress });
+            builder.Services.AddHttpClient<SurveyHttpClient>(client => client.BaseAddress = baseAddress);
+
+            builder.Services.AddSingleton<HubConnection>(sp =>
+            {
+                var navigationManager = sp.GetRequiredService<NavigationManager>();
+                return new HubConnectionBuilder()
+                    .WithUrl(navigationManager.ToAbsoluteUri("/surveyhub"))
+                    .WithAutomaticReconnect()
+                    .Build();
+            });
 
             await builder.Build().RunAsync();
         }
