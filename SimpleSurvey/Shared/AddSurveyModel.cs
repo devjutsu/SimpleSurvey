075706f563a2792e4f4c165a175bd44040f7dd54
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-09 18:54:57 +0300

    Validation

--- SimpleSurvey/Shared/AddSurveyModel.cs
+++ SimpleSurvey/Shared/AddSurveyModel.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleSurvey.Shared
 {
-    public class AddSurveyModel
+    public class AddSurveyModel : IValidatableObject
     {
+        [Required]
+        [MaxLength(50)]
         public string Title { get; set;  }
+        [Required]
         public int? Minutes { get; set; }
         public List<OptionsCreateModel> Options { get; set; } = new List<OptionsCreateModel>();
         public void RemoveOption(OptionsCreateModel option) => this.Options.Remove(option);
         public void AddOption() => this.Options.Add(new OptionsCreateModel());
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(this.Options.Count < 2)
+            {
+                yield return new ValidationResult("A survey required at least 2 options");
+            }
+        }
     }
 
     public class OptionsCreateModel
